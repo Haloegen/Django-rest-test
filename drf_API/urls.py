d4b07d6b46431c4fba7978abc0720d6d95e51557
Josh Lee <JoshCliffLee@gmail.com>
2024-04-17 22:06:06 +0000

    newer stuff

--- drf_API/urls.py
+++ drf_API/urls.py
@@ -1,4 +1,4 @@
-"""drf_api URL Configuration
+"""drf_API URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,19 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from .views import root_route, logout_route
+from django.urls import path, include 
+from .views import root_route
 
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # our logout route has to be above the default one to be matched first
-    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
-    ),
+    path('dj-rest-auth/registration', include('dj_rest_auth.registration.urls')),
     path('', include('profiles.urls')),
     path('', include('post.urls')),
     path('', include('comments.urls')),
