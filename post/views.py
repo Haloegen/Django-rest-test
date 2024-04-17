4764914550282eea22f03e203ffa58c9eb9ff3f6
Josh Lee <JoshCliffLee@gmail.com>
2024-04-17 20:48:31 +0000

    i think this almost finished

--- post/views.py
+++ post/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_API.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -7,7 +9,31 @@ from .serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
   serializer_class= PostSerializer
   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-  queryset = Post.objects.all()
+  queryset = Post.objects.annotate(
+    comments_count = Count('comment', distinct = True),
+    likes_count = Count('likes', distinct = True)
+  ).order_by('-created_at')
+  filter_backends = [
+    filters.OrderingFilter,
+    filters.SearchFilter,
+    DjangoFilterBackend,
+  ]
+  filterset_fields = [
+    'owner__username__owner__profile',
+    'likes__owner__profile',
+    'owner__profile',
+  ]
+  search_fields = [
+    'owner__username',
+    'title',
+  ]
+  ordering_fields = [
+    'comments_count', 
+    'likes_count',
+    'likes__created_at'
+  ]
+    
+
 
   def perform_create(self, serializer):
     serializer.save(owner=self.request.user)
@@ -16,4 +42,7 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
   permission_classes = [IsOwnerOrReadOnly]
   serializer_class = PostSerializer
-  queryset = Post.objects.all()
+  queryset = Post.objects.annotate(
+    comments_count = Count('comment', distinct = True),
+    likes_count = Count('likes', distinct = True)
+  ).order_by('-created_at')
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_API.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -6,10 +8,37 @@ from .serializers import ProfileSerializer
 
 class ProfileList(generics.ListAPIView):
   serializer_class= ProfileSerializer
-  queryset = Profile.objects.all()
+  queryset = Profile.objects.annotate(
+    posts_count=Count('owner__post', distinct=True),
+    followers_count = Count('owner__followed', distinct= True),
+    following_count = Count('owner__following', distinct= True)
+  ).order_by('-created_at')
+  filter_backends = [
+    filters.OrderingFilter,
+    DjangoFilterBackend,
+  ]
+  filterset_fields = [
+    'owner__following__followed__profile',
+    'owner__followed__owner__profile',
+
+
+
+  ]
+  ordering_fields = [
+    'posts_count',
+    'followers_count',
+    'following_count',
+    'owner__following__created_at',
+    'owner_followed_created_at'
+  ]
+    
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
   permission_classes = [IsOwnerOrReadOnly]
   serializer_class = ProfileSerializer
-  queryset = Profile.objects.all()
+  queryset = Profile.objects.annotate(
+    posts_count=Count('owner__post', distinct=True),
+    followers_count = Count('owner__followed', distinct= True),
+    following_count = Count('owner__following', distinct= True)
+  ).order_by('-created_at')
